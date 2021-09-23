344c298c416cb7b2f252cf14c6969e2839010ee9
riiswa <wradji64@gmail.com>
2021-09-23 18:49:48 +0200

    Try to fix gh-pages action

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './**/*.html',
+    './assets/js/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
